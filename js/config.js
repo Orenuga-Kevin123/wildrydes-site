8ad84e07942f3546b9578ba6e94cb47d4937dcfb
Orenuga-Kevin123 <kevinorenuga124@gmail.com>
2026-03-09 13:04:16 +0100

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_gOZkP2iLz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '49pskgnaa7f9o8l88bpag9ftaj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: 'k5khh59k55vj42dogb72knooj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
