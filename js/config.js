1a57d502d133b0326eac456a0766c9907ccc287a
Orenuga-Kevin123 <kevinorenuga124@gmail.com>
2026-03-09 13:33:12 +0100

    Update AWS Cognito configuration values

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_gOZkP2iLz', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1bfngq77m1vj1effd4hbi1m7e6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_YB9rgkRFl', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '726ld9v9hir1ocbg94cdve47vm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
